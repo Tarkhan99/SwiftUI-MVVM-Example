3fcf6718a1fc1d7d3d223c02857905f73d845b4b
Tarkhan <terxantahirov1@gmail.com>
2021-08-29 19:23:09 +0400

    Initial Commit

---
+++ SwiftUI-MVVM-Combine/SwiftUI_MVVM_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_CombineApp.swift
+//  SwiftUI-MVVM-Combine
+//
+//  Created by Tarkhan Tahirov on 29.08.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-MVVM-CombineTests/SwiftUI_MVVM_CombineTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUI_MVVM_CombineTests.swift
+//  SwiftUI-MVVM-CombineTests
+//
+//  Created by Tarkhan Tahirov on 29.08.21.
+//
+
+import XCTest
+@testable import SwiftUI_MVVM_Combine
+
+class SwiftUI_MVVM_CombineTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
